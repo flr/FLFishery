cf56c30574e774af87586dc0c99830e32a4d0380
iagomosqueira <iago@mosqueira.eu>
2015-05-28 17:46:59 +0200

    Coercion to FLStock from FLB and FLF

--- R/coerce.R
+++ R/coerce.R
@@ -6,15 +6,45 @@
 # Soundtrack:
 # Notes:
 
-# FLStock {{{
+# FLStock  -> FLCatch {{{
 
 setAs('FLStock', 'FLCatch',
 	function(from)
 	{
 		FLCatch(landings.n=landings.n(from), landings.wt=landings.wt(from),
 			discards.n=discards.n(from), discards.wt=discards.wt(from),
-			catch.sel=harvest(from) %/% apply(harvest(from), 2:6, max))
+			catch.sel=catch.sel(from))
 	}
 )
 # }}}
+
+# FLStock  -> FLFishery {{{
+
+setAs('FLStock', 'FLFishery',
+	function(from)
+	{
+		FLFishery(as(from, 'FLCatch'), effort=catch(from) / esb(from))
+	}
+)
+# }}}
+
+
+asFLStock <- function(fbi, fca) {
+	
+	res <- FLStock(
+		# From FLBiol
+		stock.n = n(fbi), stock.wt=wt(fbi),
+		m=m(fbi), m.spwn=spwn(fbi), mat=fec(fbi),
+
+		# From FLFishery
+		landings.n=landings.n(fca), landings.wt=landings.wt(fca),
+		discards.n=discards.n(fca), discards.wt=discards.wt(fca)
+	)
+
+	#
+	stock(res) <- computeStock(res)
+	
+	catch(res) <- computeCatch(res, 'all')
+
+}
 
