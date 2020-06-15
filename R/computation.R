25be6b4dab6305fa366894e5665c52c6b9091f14
Iago Mosqueira <iago@mosqueira.eu>
2020-06-15 16:35:20 +0200

    Completed methods to get totals at fishery/ies levels

--- R/computation.R
+++ R/computation.R
@@ -6,7 +6,89 @@
 #
 # Distributed under the terms of the EUPL-1.2
 
+# landings (FLC, FLF) {{{
+
+#' @rdname FLCatch
+setMethod("landings", signature(object="FLCatch"),
+  function(object) {
+    return(quantSums(landings.wt(object) * landings.n(object)))
+  }
+)
+
+#' @rdname FLFishery
+setMethod("landings", signature(object="FLFishery"),
+  function(object, catch=seq(object)) {
+    return(Reduce("%+%", lapply(object, "landings")[catch]))
+  }
+) # }}}
+
+# discards (FLC, FLF) {{{
+#' @rdname FLCatch
+setMethod("discards", signature(object="FLCatch"),
+  function(object) {
+    return(quantSums(discards.n(object) * discards.wt(object)))
+  }
+)
+
+#' @rdname FLFishery
+setMethod("discards", signature(object="FLFishery"),
+  function(object, catch=names(object)) {
+    return(Reduce("%+%", lapply(object, "discards")[catch]))
+  }
+) # }}}
+
+# catch (FLC, FLF, FLFs) {{{
+#' @rdname FLCatch
+setMethod("catch", signature(object="FLCatch"),
+  function(object) {
+    return(landings(object) + discards(object))
+  }
+)
+
+#' @rdname FLFishery
+setMethod("catches", signature(object="FLFishery"),
+  function(object) {
+    res <- lapply(object@.Data, catch)
+    names(res) <- names(object)
+    return(FLQuants(res))
+  }
+)
+
+#' @rdname FLFishery
+setMethod("catch", signature(object="FLFishery"),
+  function(object) {
+    return(Reduce("%+%", lapply(object, "catch")))
+  }
+) 
+
+#' @rdname FLFisheries
+setMethod("catch", signature(object="FLFisheries"),
+  function(object) {
+    return(Reduce("%+%", lapply(object, "catch")))
+  }
+) # }}}
+
+# landings.n (FLF, FLs) {{{
+setMethod("landings.n", signature(object="FLFishery"),
+  function(object, pos=names(object)) {
+    if(length(pos) == 1)
+      lapply(object, "landings.n")[[pos]]
+    else
+      lapply(object, "landings.n")[pos]
+  }
+)
+
+setMethod("landings.n", signature(object="FLFisheries"),
+  function(object, pos=lapply(object, names)) {
+    if(length(pos) == 1)
+      FLQuants(mapply("landings.n", object, pos, SIMPLIFY=FALSE))
+    else
+      mapply("landings.n", object, pos, SIMPLIFY=FALSE)
+  }
+) # }}}
+
 # catch.n (FLC, FLF, FLFs) {{{
+
 #' @rdname FLCatch
 setMethod("catch.n", signature(object="FLCatch"),
   function(object) {
@@ -24,6 +106,7 @@ setMethod("catch.n", signature(object="FLFishery"),
   }
 )
 
+#' @rdname FLFisheries
 setMethod("catch.n", signature(object="FLFisheries"),
   function(object, pos=lapply(object, names)) {
     if(length(pos) == 1)
@@ -31,27 +114,35 @@ setMethod("catch.n", signature(object="FLFisheries"),
     else
       mapply("catch.n", object, pos, SIMPLIFY=FALSE)
   }
+) # }}}
+
+# catch.wt (FLC, FLF, FLFs) {{{
+
+#' @rdname FLCatch
+setMethod("catch.wt", signature(object="FLCatch"),
+  function(object) {
+    return(landings.n(object) + discards.n(object))
+  }
 )
 
-# }}}
-
-# landings.n (FLF, FLs)
-setMethod("landings.n", signature(object="FLFishery"),
+#' @rdname FLFishery
+setMethod("catch.wt", signature(object="FLFishery"),
   function(object, pos=names(object)) {
     if(length(pos) == 1)
-      lapply(object, "landings.n")[[pos]]
+      lapply(object, "catch.wt")[[pos]]
     else
-      lapply(object, "landings.n")[pos]
+      lapply(object, "catch.wt")[pos]
   }
 )
 
-setMethod("landings.n", signature(object="FLFisheries"),
+#' @rdname FLFisheries
+setMethod("catch.wt", signature(object="FLFisheries"),
   function(object, pos=lapply(object, names)) {
     if(length(pos) == 1)
-      FLQuants(mapply("landings.n", object, pos, SIMPLIFY=FALSE))
+      FLQuants(mapply("catch.wt", object, pos, SIMPLIFY=FALSE))
     else
-      mapply("landings.n", object, pos, SIMPLIFY=FALSE)
+      mapply("catch.wt", object, pos, SIMPLIFY=FALSE)
   }
-)
+) # }}}
 
 
