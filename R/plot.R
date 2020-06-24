f7a4ca119ab15fed28362aca675a466ad50857e8
Iago Mosqueira <iago@mosqueira.eu>
2020-06-24 09:39:37 +0200

    plot(FLIndices) show the catch and effort series by fishery and catch.

--- R/plot.R
+++ R/plot.R
@@ -51,7 +51,42 @@ setMethod("plot", signature(x="FLFishery"),
     facet_grid(panel~., scales="free_y", labeller=labels) +
     theme() +
     ylim(c(0, NA)) + ylab("") + xlab("")
+
   }) # }}}
+
+# FLFisheries {{{
+
+setMethod("plot", signature(x="FLFisheries"),
+  function(x) {
+
+    # PLOT catch by fleet ~ stock
+
+    # EXTRACT FLQuants
+    eff <- lapply(x, effort)
+    cas <- lapply(x, lapply, catch)
+
+    # ADD units to FLQuants names
+    cas <- lapply(cas, function(x) {
+      names(x) <- paste0(names(x), " (", unlist(lapply(x, units)), ")")
+      return(x)
+    })
+
+    # GENERATE metrics as catches + eff
+    mets <- mapply(function(x, y) FLQuants(c(x, list(effort=y))), cas, eff, SIMPLIFY=FALSE)
+    dfs <- lapply(mets, as.data.frame, date=TRUE)
+
+    # DATA with qname (eff, SPP) and fishery
+    dat <- do.call(rbind,
+      c(mapply(cbind, dfs, fishery=names(mets), SIMPLIFY=FALSE),
+      list(make.row.names = FALSE)))
+
+    ggplot(dat, aes(x=date, y=data)) + geom_line(aes(colour=fishery)) +
+      facet_grid(qname ~ fishery, scales="free_y") +
+      xlab("") + ylab("") + ylim(c(0, NA)) +
+      theme(legend.position="none")
+
+  }
+) # }}}
 
 # FLBiol, FLFishery
 # FLBiol, FLFisheries
