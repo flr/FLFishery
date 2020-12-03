8d4eb69996b268f5ff2d3f80bd5eb3b44eee5d5e
Iago Mosqueira <iago@mosqueira.eu>
2020-12-03 11:47:50 +0100

    Tests for methods to compute FLCatch quantities for FLFishery and FLFisheries

--- tests/testthat/test-computation.R
+++ tests/testthat/test-computation.R
@@ -6,14 +6,68 @@
 #
 # Distributed under the terms of the EUPL-1.2
 
+context("FLC/FLF/FLFs slot computation")
 
-# nsfleet, ple, sol
+# LOAD nsfleet (FLFs), ple (FLB), sol (FLB)
 
 data(nsfishery)
 
-# catch, catches
+# TEST landings (FLC / FLF / FLFs){{{
 
-# catch.n
+test_that("landings(FLFs/FLF) returns FLQs, landings(FLC) an FLQ", {
+  
+  # FLFs
+  # output is an FLQuants 
+  expect_s4_class(landings(nsfleet), "FLQuants")
 
-names(catch.n(nsfleet))
-is(catch.n(nsfleet)[[1]])
+  # dimnames match input FLQuant
+  expect_equal(unique(unlist(lapply(nsfleet, names))), names(landings(nsfleet)))
+
+  # FLF
+  expect_s4_class(landings(nsfleet[[1]]), "FLQuants")
+  expect_equal(names(nsfleet[[1]]), names(landings(nsfleet[[1]])))
+
+  # FLC
+  expect_s4_class(landings(nsfleet[[1]][[1]]), "FLQuant")
+
+}) # }}}
+
+# TEST discards (FLC / FLF / FLFs){{{
+
+test_that("discards(FLFs/FLF) returns FLQs, discards(FLC) an FLQ", {
+  
+  # FLFs
+  # output is an FLQuants 
+  expect_s4_class(discards(nsfleet), "FLQuants")
+
+  # dimnames match input FLQuant
+  expect_equal(unique(unlist(lapply(nsfleet, names))), names(discards(nsfleet)))
+
+  # FLF
+  expect_s4_class(discards(nsfleet[[1]]), "FLQuants")
+  expect_equal(names(nsfleet[[1]]), names(discards(nsfleet[[1]])))
+
+  # FLC
+  expect_s4_class(discards(nsfleet[[1]][[1]]), "FLQuant")
+
+}) # }}}
+
+# TEST catch (FLC / FLF / FLFs){{{
+
+test_that("catch(FLFs/FLF) returns FLQs, catch(FLC) an FLQ", {
+  
+  # FLFs
+  # output is an FLQuants 
+  expect_s4_class(catch(nsfleet), "FLQuants")
+
+  # dimnames match input FLQuant
+  expect_equal(unique(unlist(lapply(nsfleet, names))), names(catch(nsfleet)))
+
+  # FLF
+  expect_s4_class(catch(nsfleet[[1]]), "FLQuants")
+  expect_equal(names(nsfleet[[1]]), names(catch(nsfleet[[1]])))
+
+  # FLC
+  expect_s4_class(catch(nsfleet[[1]][[1]]), "FLQuant")
+
+}) # }}}
